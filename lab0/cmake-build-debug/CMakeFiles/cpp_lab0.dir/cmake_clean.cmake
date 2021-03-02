e0e63b86ff0800bd4770afdf16441fb9ab3fba2c
fhideous <kul.leshiy@yandex.ru>
2021-03-02 03:49:41 +0300

    lab 0 check

---
+++ lab0/cmake-build-debug/CMakeFiles/cpp_lab0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_lab0.dir/src/lab0_support.cpp.o"
+  "CMakeFiles/cpp_lab0.dir/src/main.cpp.o"
+  "cpp_lab0"
+  "cpp_lab0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_lab0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
